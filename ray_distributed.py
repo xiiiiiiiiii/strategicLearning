a0adfa5662cb8ba45013de007c337e7d054b82c2
xiiiiiiiiii <xiiiiiiiiii@gmail.com>
2025-03-10 17:34:16 -0700

    debug

--- ray_distributed.py
+++ ray_distributed.py
@@ -115,7 +115,7 @@ class LLMPredictor:
 def scheduling_strategy_fn():
     # One bundle per tensor parallel worker
     pg = ray.util.placement_group(
-        [[{"GPU": 1, }] * (tensor_parallel_size * num_instances), [{"CPU": 1}] * 1],
+        [[{"GPU": 1, "CPU": 0}] * (tensor_parallel_size * num_instances), [{"GPU": 0, "CPU": 1}] * 1],
         strategy="STRICT_PACK",
     )
     return dict(scheduling_strategy=PlacementGroupSchedulingStrategy(
