51832a881a0b3d2bc0c1bc753bf98bb64e3d09db
xiiiiiiiiii <xiiiiiiiiii@gmail.com>
2025-03-10 17:15:30 -0700

    debug

--- ray_distributed.py
+++ ray_distributed.py
@@ -117,7 +117,7 @@ def scheduling_strategy_fn():
         [{
             "GPU": 2,
             "CPU": 1
-        }] * (tensor_parallel_size * num_instances),
+        }] * (tensor_parallel_size),
         strategy="STRICT_PACK",
     )
     return dict(scheduling_strategy=PlacementGroupSchedulingStrategy(
@@ -132,7 +132,7 @@ resources_kwarg: dict[str, Any] = {}
 # Otherwise, we have to set num_gpus=0 and provide
 # a function that will create a placement group for
 # each instance.
-resources_kwarg["num_gpus"] = num_instances
+resources_kwarg["num_gpus"] = 1
 resources_kwarg["ray_remote_args_fn"] = scheduling_strategy_fn
 
 # Apply batch inference for all input data.
