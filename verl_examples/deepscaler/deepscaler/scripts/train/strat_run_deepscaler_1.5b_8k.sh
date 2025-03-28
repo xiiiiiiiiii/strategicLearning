0a2383829de520470774ccb5799aea35387a723d
xiiiiiiiiii <xiiiiiiiiii@gmail.com>
2025-03-27 23:19:41 -0700

    First version training on tweak data

--- verl_examples/deepscaler/deepscaler/scripts/train/strat_run_deepscaler_1.5b_8k.sh
+++ verl_examples/deepscaler/deepscaler/scripts/train/strat_run_deepscaler_1.5b_8k.sh
@@ -24,6 +24,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=16 \
+    actor_rollout_ref.actor.ppo_micro_batch_size=16 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=32768 \
     actor_rollout_ref.actor.use_kl_loss=True \
