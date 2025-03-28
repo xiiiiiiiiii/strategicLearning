cf89222c76a4b5bd80179d95d687fbfa0b4e2258
xiiiiiiiiii <xiiiiiiiiii@gmail.com>
2025-03-27 18:13:04 -0700

    Config to run deepscaler on latest verl

--- verl_examples/deepscaler/run_deepscaler_1.5b_8k.sh
+++ verl_examples/deepscaler/run_deepscaler_1.5b_8k.sh
@@ -6,19 +6,23 @@ set -x
 
 MODEL_PATH="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
 
+# Need to specify custom reward function.
+
 # Train over a single node, 8 A100-80GB GPUs.
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=$HOME/deepscaler/data/train.parquet \
     data.val_files=$HOME/deepscaler/data/aime.parquet \
-    data.train_batch_size=128 \
-    data.val_batch_size=512 \
+    data.train_batch_size=32 \
+    data.val_batch_size=128 \
     data.max_prompt_length=1024 \
     data.max_response_length=8192 \
     actor_rollout_ref.model.path=$MODEL_PATH  \
+    custom_reward_function.path=deepscaler_math_reward.py \
+    custom_reward_function.name=deepscaler_reward_fn \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=64 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=16 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=32768 \
     actor_rollout_ref.actor.use_kl_loss=True \
@@ -34,8 +38,8 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.temperature=0.6 \
     actor_rollout_ref.rollout.val_temperature=0.6 \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.85 \
-    actor_rollout_ref.rollout.n=8 \
-    actor_rollout_ref.rollout.n_val=8 \
+    actor_rollout_ref.rollout.n=2 \
+    actor_rollout_ref.rollout.n_val=2 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
@@ -43,7 +47,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.project_name='deepscaler' \
     trainer.experiment_name='deepscaler-1.5b-8k' \
     +trainer.val_before_train=True \
-    trainer.n_gpus_per_node=8 \
+    trainer.n_gpus_per_node=2 \
     trainer.nnodes=1 \
     trainer.save_freq=20 \
     trainer.test_freq=20 \
