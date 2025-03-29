3c91523c546c9430cba54f449eefd8b7ed1b6efa
xiiiiiiiiii <xiiiiiiiiii@gmail.com>
2025-03-29 13:53:25 -0700

    tweak

--- strat_run_deepscaler_1.5b_24k-n8.sh
+++ strat_run_deepscaler_1.5b_24k-n8.sh
@@ -20,8 +20,8 @@ python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=./train.parquet \
     data.val_files=./aime.parquet \
-    data.train_batch_size=64 \
-    data.val_batch_size=64 \
+    data.train_batch_size=32 \
+    data.val_batch_size=32 \
     data.max_prompt_length=1024 \
     data.max_response_length=24576 \
     actor_rollout_ref.model.path=$MODEL_PATH  \
