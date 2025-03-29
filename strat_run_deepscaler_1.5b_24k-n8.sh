bd9328f8e71452b20e76c8f960f2dae0c1c1d734
xiiiiiiiiii <xiiiiiiiiii@gmail.com>
2025-03-29 14:21:14 -0700

    4xH100 config

--- strat_run_deepscaler_1.5b_24k-n8.sh
+++ strat_run_deepscaler_1.5b_24k-n8.sh
@@ -15,7 +15,8 @@ export VLLM_ATTENTION_BACKEND=XFORMERS
 
 # Need to specify custom reward function.
 
-# Train over a single node, with 4 H100-80GB GPUs.
+# Train over a single node, with 8 H100-80GB GPUs.
+# Doesn't seem worth the 2x cost compared to 4 GPUs.
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=./train.parquet \
