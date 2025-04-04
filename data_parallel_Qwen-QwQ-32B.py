683e7bc701d86e12d9d366aa8948e7b3e4d6c14a
xiiiiiiiiii <xiiiiiiiiii@gmail.com>
2025-04-04 15:14:13 -0700

    tweaks

--- data_parallel_Qwen-QwQ-32B.py
+++ data_parallel_Qwen-QwQ-32B.py
@@ -39,6 +39,14 @@ sampling_params = SamplingParams(
     max_tokens=32767
 )
 
+rope_scaling_params = {
+    "rope_scaling": {
+        "type": "yarn",
+        "factor": 4.0,
+        "original_max_position_embeddings": 32768
+    }
+}
+
 model = args.model
 run_name = args.run_name
 dataset_to_gen = args.dataset
@@ -177,6 +185,7 @@ def main(dp_size, dp_rank, dp_master_ip, dp_master_port, GPUs_per_dp_rank):
     llm = LLM(
         model=model,
         tensor_parallel_size=GPUs_per_dp_rank,
+        **rope_scaling_params
         # enforce_eager=True
     )
 
