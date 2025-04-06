6c6c15ca1f54a715b5d6efcdb97cae8417f274a9
xiiiiiiiiii <xiiiiiiiiii@gmail.com>
2025-04-05 20:41:18 -0700

    tweaks

--- top_600_ripe_0_similar/ripe-0-similar-ripe-sft-grpo/fsdp_sft_trainer_deepscaler_1.5b-n2.sh
+++ top_600_ripe_0_similar/ripe-0-similar-ripe-sft-grpo/fsdp_sft_trainer_deepscaler_1.5b-n2.sh
@@ -10,12 +10,12 @@ torchrun --standalone --nnodes=1 --nproc_per_node=$nproc_per_node \
     data.train_files=./train.parquet \
     data.val_files=./train.parquet \
     data.max_length=24576 \
-    data.micro_batch_size=1
+    data.micro_batch_size=1 \
     data.prompt_key=extra_info \
     data.response_key=extra_info \
     +data.prompt_dict_keys=['question'] \
     +data.response_dict_keys=['answer'] \
-    model.partial_pretrain=agentica-org/DeepScaleR-1.5B-Preview \
+    model.partial_pretrain="${MODEL_PATH}" \
     trainer.default_local_dir=./model_save_path \
     trainer.project_name='deepscaler-finetune' \
     trainer.experiment_name="${EXPERIMENT_NAME}" \
