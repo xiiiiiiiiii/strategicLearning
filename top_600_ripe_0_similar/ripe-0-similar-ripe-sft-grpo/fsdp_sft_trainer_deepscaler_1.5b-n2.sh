315536d66a81a66dac70a1b0092168718e00adb2
xiiiiiiiiii <xiiiiiiiiii@gmail.com>
2025-04-05 21:49:09 -0700

    tweaks

--- top_600_ripe_0_similar/ripe-0-similar-ripe-sft-grpo/fsdp_sft_trainer_deepscaler_1.5b-n2.sh
+++ top_600_ripe_0_similar/ripe-0-similar-ripe-sft-grpo/fsdp_sft_trainer_deepscaler_1.5b-n2.sh
@@ -3,15 +3,15 @@
 MODEL_PATH="agentica-org/DeepScaleR-1.5B-Preview"
 EXPERIMENT_NAME="deepscaler-sft-1.5b"
 
-nproc_per_node=1
+nproc_per_node=2
 
 torchrun --standalone --nnodes=1 --nproc_per_node=$nproc_per_node \
      -m verl.trainer.fsdp_sft_trainer \
     data.train_files=./train.parquet \
     data.val_files=./train.parquet \
     data.max_length=24576 \
-    data.train_batch_size=1 \
-    data.micro_batch_size=1 \
+    data.train_batch_size=2 \
+    data.micro_batch_size=2 \
     data.prompt_key=extra_info \
     data.response_key=extra_info \
     +data.prompt_dict_keys=['question'] \
