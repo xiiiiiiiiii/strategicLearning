38e2ce7bfdff1ed42413bd7933a1903885f98549
xiiiiiiiiii <xiiiiiiiiii@gmail.com>
2025-04-12 16:06:18 -0700

    tweaks

--- top_600_s1-full-59k_similar/all-epochs-30/strat_run_deepscaler_1.5b_24k-n4.sh
+++ top_600_s1-full-59k_similar/all-epochs-30/strat_run_deepscaler_1.5b_24k-n4.sh
@@ -45,7 +45,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.val_temperature=0.6 \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.85 \
     actor_rollout_ref.rollout.n=16 \
-    actor_rollout_ref.rollout.n_val=16 \
+    actor_rollout_ref.rollout.n_val=32 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
