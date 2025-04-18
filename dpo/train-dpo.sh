c06ed5333989b51e29c8200e56dce3d5107ec48d
xiiiiiiiiii <xiiiiiiiiii@gmail.com>
2025-04-17 21:53:53 -0700

    Working version of dpo training

--- dpo/train-dpo.sh
+++ dpo/train-dpo.sh
@@ -3,7 +3,7 @@ MODEL_PATH="agentica-org/DeepScaleR-1.5B-Preview"
 EXPERIMENT_NAME="deepscaler-dpo-finetune-1.5b-24k"
 
 # Light-R1 DPO used 360-LLaMA-Factory directly
-deepspeed src/train.py \
+deepspeed --num_gpus=8 src/train.py \
     --stage dpo \
     --do_train \
     --max_steps -1 \
@@ -22,13 +22,13 @@ deepspeed src/train.py \
     --cutoff_len 24576 \
     --output_dir $EXPERIMENT_NAME \
     --per_device_train_batch_size 1 \
-    --gradient_accumulation_steps 4 \
+    --gradient_accumulation_steps 1 \
     --lr_scheduler_type constant \
     --save_strategy steps \
     --save_steps 2 \
     --logging_steps 1 \
     --warmup_ratio 0.0 \
-    --save_total_limit 2 \
+    --save_total_limit 10 \
     --learning_rate 5e-7 \
     --save_only_model True \
     --num_train_epochs 3.0 \
